e232698c4da34ffd05be0dbfa655c56869996285
Ricardo Zapata Cruz <ricardozapatacruz@gmail.com>
2025-11-24 14:21:36 -0500

    Fix sass and bootstrap error

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: ['legacy-js-api'],
+        quietDeps: true,
+      },
+    },
+  },
   plugins: [react()],
 })
