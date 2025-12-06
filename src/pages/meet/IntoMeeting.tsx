2c9671de9b4a8f4bffacdedec004fcdc9b106ae2
Ricardo Zapata Cruz <ricardozapatacruz@gmail.com>
2025-12-06 15:26:49 -0500

    Delete duplicate cards

--- src/pages/meet/IntoMeeting.tsx
+++ src/pages/meet/IntoMeeting.tsx
@@ -258,9 +258,7 @@ const IntoMeeting: React.FC = () => {
         <section className="participants-grid">
           {/* Card principal - usuario local (más grande) */}
           {localSocketId && participants.find(p => p.id === localSocketId) && (
-            <>
-              <MeetCard {...participants.find(p => p.id === localSocketId)!} /><MeetCard {...participants.find(p => p.id === localSocketId)!} />
-            </>
+            <MeetCard {...participants.find(p => p.id === localSocketId)!} />
           )}
 
           {/* Grid de cards secundarias - otros participantes (más pequeñas) */}
