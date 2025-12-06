c0c0a59040a04e31c8e65aa80ec993010299a187
Ricardo de Jesus Zapata Cruz <ricardozapatacruz@gmail.com>
2025-12-05 21:49:40 -0500

    Ricardo (#25)
    * Responsive meeting
    
    * Auth and home responsive

--- src/components/headers/HomeHeader.tsx
+++ src/components/headers/HomeHeader.tsx
@@ -1,17 +1,26 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./HomeHeader.scss";
+import { Menu } from "lucide-react";
 
 const HomeHeader: React.FC = () => {
+  const navigate = useNavigate();
   return (
     <header className="home-header">
       <Link to="/">
         <img className="logo-img" src="/logo.png" alt="VidSync" />
       </Link>
 
-      <div className="home-header-buttons">
+      <section className="login-buttons">
         <Link to="/login" className="btn btn-primary">Iniciar sesión</Link>
         <Link to="/register" className="btn btn-white">Regístrate</Link>
+      </section>
+      <div className="dropdown">
+        <button className="btn btn-icon color-primary" type="button" data-bs-toggle="dropdown" aria-expanded="false"><Menu /></button>
+        <ul className="dropdown-menu dropdown">
+          <li><a className="dropdown-item" onClick={() => navigate('/login')}>Iniciar Sesión</a></li>
+          <li><a className="dropdown-item" onClick={() => navigate('/register')}>Registrarse</a></li>
+        </ul>
       </div>
     </header>
   );
